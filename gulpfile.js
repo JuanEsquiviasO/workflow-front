9c00d5d3467aade41be63abc2e7949b77e3167c2
Juan Esquivias <juanesquiviasotazu@gmail.com>
2017-02-10 16:05:27 -0500

    Gulp working with Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 
 gulp.task('default', function(){
 	console.log('Hi world!!!');
+});
+
+gulp.task('sass', function(){
+	return gulp.src('scss/main.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('app/css'));
 });
