80ca17f5a274804b486435b7f174d9c5dc13ad26
Juan Esquivias <juanesquiviasotazu@gmail.com>
2017-02-18 12:13:30 -0500

    Gulp and CSSnano

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
 const uglify = require('gulp-uglify');
 const pump = require('pump');
+const cssnano = require('gulp-cssnano');
+const browserSync = require('browser-sync').create();
 
 gulp.task('serve', ['sass'], function(){
 	browserSync.init({
@@ -27,6 +28,7 @@ gulp.task('compress', function(cb){
 gulp.task('sass', function(){
 	return gulp.src('scss/**/*.scss')
 		.pipe(sass())
+		.pipe(cssnano())
 		.pipe(gulp.dest('app/css'))
 		.pipe(browserSync.stream());
 });
